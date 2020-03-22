750a65ca4dd5d74720f486a6602741ba00eec8b5
Jack Kamm <jackkamm@gmail.com>
2020-03-22 10:59:18 -0700

    Fix primer bed file

--- scripts/get_primers_from_fasta.py
+++ scripts/get_primers_from_fasta.py
@@ -2,22 +2,19 @@ from Bio import SeqIO
 
 primers = list(SeqIO.parse("data/SARS-COV-2_spikePrimers.fasta", "fasta"))
 
-primers_rc = []
-for s in primers:
-    primers_rc.append("".join(s.reverse_complement()))
-
 ref, = SeqIO.parse("data/MN908947.3.fa", "fasta")
 ref_str = "".join(ref)
 
-assert all([s in ref_str for s in primers_rc])
+primer_pos = []
+for s in primers:
+    rc = "".join(s.reverse_complement())
+    assert rc in ref_str
+    primer_pos.append(ref_str.find(rc))
 
-primer_pos = [ref_str.find(s) for s in primers_rc]
+sorted_pos_seq = sorted(zip(primer_pos, primers), key=lambda x: x[0])
 
 chrom = "MN908947.3"
 with open("data/SARS-COV-2_spikePrimers.bed", "w") as f:
-    for i in range(len(primers)):
-        name = primers
-        rc = primers_rc[i]
-        pos = primer_pos[i]
-        print(chrom, pos, pos + len(rc),
-              primers[i].name, 60, "-", file=f, sep="\t")
+    for pos, seq in sorted_pos_seq:
+        print(chrom, pos, pos + len(seq),
+              seq.name, 60, "+", file=f, sep="\t")
