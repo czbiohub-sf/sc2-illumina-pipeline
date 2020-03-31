3912ad47a9005609d7b7f8178e7fd5eaf1602563
Samantha Hao <s.hao@berkeley.edu>
2020-03-31 15:50:34 -0700

    fixed script

--- scripts/parse_quast_unaligned_report.py
+++ scripts/parse_quast_unaligned_report.py
@@ -7,16 +7,16 @@ from Bio import SeqIO
 import shutil
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', help='quast unaligned_report.tsv file')
-parser.add_argument('-n', help='max number of Ns to allow')
-parser.add_argument('-a', help='assembly FASTA')
-parser.add_argument('-l', help='min length for passing QC')
+parser.add_argument('--file', help='quast unaligned_report.tsv file')
+parser.add_argument('-n', help='max number of Ns to allow', type=int, default=100)
+parser.add_argument('--assembly', help='assembly FASTA')
+parser.add_argument('-l', help='min length for passing QC', type=int, default=29000)
 args = parser.parse_args()
 
-df = pd.read_csv(args.f, sep='\t')
-num_Ns = df.iloc[4, 1]
+df = pd.read_csv(args.file, sep='\t')
+num_Ns = df.iloc[4, 1][0]
 
-seqrecords = SeqIO.parse(args.a, 'fasta')
+seqrecords = SeqIO.parse(args.assembly, 'fasta')
 seq_length = sum(len(s) for s in seqrecords)
 
 if num_Ns <= args.n and seq_length >= args.l:
