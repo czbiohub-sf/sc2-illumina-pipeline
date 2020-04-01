efe8005528f72f2d4662e4f9e273921fef02c9d8
samlhao <samantha.hao@czbiohub.org>
2020-04-01 08:48:04 -0700

    sort assemblies by QC

--- bin/parse_quast_unaligned_report.py
+++ bin/parse_quast_unaligned_report.py
@@ -3,21 +3,21 @@
 
 import pandas as pd
 import argparse
-from Bio import SeqIO
 import shutil
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--file', help='quast unaligned_report.tsv file')
+parser.add_argument('--unaligned_report', help='quast unaligned_report.tsv')
+parser.add_argument('--report', help='quast report.tsv')
 parser.add_argument('-n', help='max number of Ns to allow', type=int, default=100)
 parser.add_argument('--assembly', help='assembly FASTA')
 parser.add_argument('-l', help='min length for passing QC', type=int, default=29000)
 args = parser.parse_args()
 
-df = pd.read_csv(args.file, sep='\t')
+df = pd.read_csv(args.unaligned_report, sep='\t')
 num_Ns = df.iloc[4, 1]
 
-seqrecords = SeqIO.parse(args.assembly, 'fasta')
-seq_length = sum(len(s) for s in seqrecords)
+report_df = pd.read_csv(args.report, sep='\t')
+seq_length = int(report_df.iloc[6,1])
 
 if num_Ns <= args.n and seq_length >= args.l:
 	shutil.copy(args.assembly, 'passed_QC/')
